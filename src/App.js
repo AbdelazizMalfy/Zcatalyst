389def8bb213d882a476e718168e653c51cb25b5
Abdelaziz <abdelazizmalfy@gmail.com>
2020-08-30 12:51:29 +0200

    add header styles, add react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-
-      {/* Header */}
       <Header />
+      <Router>
+        <Switch>
+          <Route path="/chat">
+            <h1>I am chat page</h1>
+          </Route>
+          <Route path="/">
+            <h1>I am home page</h1>
+          </Route>
+        </Switch>
+      </Router>
+      
       {/* Tinder Cards */}
       {/* Buttons */}
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import './Header.css'
 import PersonIcon from '@material-ui/icons/Person'
 import ForumIcon from '@material-ui/icons/Forum'
+import IconButton from '@material-ui/core/IconButton'
 
 const Header = () => {
   return (
-    <div>
-      <PersonIcon />
-      <ForumIcon />
+    <div className="header">
+      <IconButton>
+        <PersonIcon className="header__icon" fontSize="large" />
+      </IconButton>
+      <a href="/" className="header__logo">🧪</a>
+      <IconButton>
+        <ForumIcon className="header__icon" fontSize="large" />
+      </IconButton>
     </div>
   )
 }
